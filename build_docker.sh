23dc00ccd810fa6dafc7b8f46ccdd5d6e6a20707
Peter Gill <peter.gill@townsuite.com>
2021-06-30 17:01:40 -0230

    docker build script and custom docker image based on ubuntu 20.04.

---
+++ build_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e # exit on first error
+set -u # exit on using unset variable
+
+DOCKER_REGISTRY=$1
+echo "Using docker registry $DOCKER_REGISTRY"
+echo "Building htmlconverterwebapi:latest"
+docker build -f ./HtmlConverter.WebApi/Dockerfile -t htmlconverterwebapi --rm=true -m 2GB .
+cd ..
+
+GITHASH="$(git rev-parse --short HEAD)"
+
+echo 'Tagging latest'
+docker tag htmlconverterwebapi $DOCKER_REGISTRY/htmlconverterwebapi:latest
+echo "Tagging $GITHASH"
+docker tag htmlconverterwebapi $DOCKER_REGISTRY/htmlconverterwebapi:$GITHASH
+
+echo 'Pushing'
+docker push $DOCKER_REGISTRY/htmlconverterwebapi:latest
+docker push $DOCKER_REGISTRY/htmlconverterwebapi:$GITHASH
